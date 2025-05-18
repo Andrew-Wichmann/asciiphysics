fd413880909513f32b65c81987ba9b30f273e37f
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-05-18 14:39:33 -0400

    feat: switch to TheZoraiz/ascii-image-converter

--- canvas.go
+++ canvas.go
@@ -1,17 +1,37 @@
 package asciiphysics
 
 import (
+	"fmt"
 	"image"
+	"strings"
 	"time"
 
+	imgManip "github.com/TheZoraiz/ascii-image-converter/image_manipulation"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fogleman/gg"
 	"github.com/qeesung/image2ascii/convert"
 )
 
 const (
-	fps = 60
+	fps = 30
 )
+
+func flattenAscii(asciiSet [][]imgManip.AsciiChar) string {
+	var ascii []string
+
+	for _, line := range asciiSet {
+		var tempAscii string
+
+		for _, char := range line {
+			tempAscii += char.OriginalColor
+		}
+
+		ascii = append(ascii, tempAscii)
+	}
+	result := strings.Join(ascii, "\n")
+
+	return result
+}
 
 type canvasTick struct{}
 type Drawable interface {
@@ -50,7 +70,16 @@ func (c Canvas) View() string {
 	for _, drawable := range c.drawable {
 		drawable.Draw(ctx)
 	}
-	return c.asciiConverter.Image2ASCIIString(ctx.Image(), &convert.DefaultOptions)
+	dimensions := []int{c.width, c.height}
+	imgSet, err := imgManip.ConvertToAsciiPixels(ctx.Image(), dimensions, c.width, c.height, false, false, false, false, true)
+	if err != nil {
+		return fmt.Sprintf("Error rendering canvas: %s", err.Error())
+	}
+	asciiSet, err := imgManip.ConvertToAsciiChars(imgSet, false, true, false, true, false, "", [3]int{0, 0, 0})
+	if err != nil {
+		return fmt.Sprintf("Error rendering canvas: %s", err.Error())
+	}
+	return flattenAscii(asciiSet)
 }
 
 func (c *Canvas) AddDrawable(drawable Drawable) {
